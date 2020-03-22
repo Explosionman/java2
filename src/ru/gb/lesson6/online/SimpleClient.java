fa52441fd2a472439af6e09b34ff6d5bf1cc4b9f
Alexandr Rybinskov <rybinskov@gmail.com>
2020-03-22 19:36:07 +0300

    first commit

---
+++ src/ru/gb/lesson6/online/SimpleClient.java
@@ -0,0 +1,26 @@
+package ru.gb.lesson6.online;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SimpleClient {
+    public static void main(String[] args) {
+        try (Socket s = new Socket("127.0.0.1", 8189)) {
+            System.out.println("Socked connected");
+            DataInputStream in = new DataInputStream(s.getInputStream());
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            System.out.println("Socked ready");
+            Scanner sc = new Scanner(System.in);
+            while (true) {
+                out.writeUTF(sc.nextLine());
+                System.out.println("Byte written");
+                String b = in.readUTF();
+                System.out.println("Byte received: " + b);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
